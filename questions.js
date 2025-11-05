a2894652e9e53237ee811e5e1ce2a088624d1060
ajuururuta <bvlbymusic@163.com>
2025-11-05 13:55:53 +0800

    questions.js

---
+++ questions.js
@@ -0,0 +1,22 @@
+// 定义题目数组
+const questions = [
+    {
+        question: "函数 f(x) = x² 的最小值是多少？",
+        answers: [
+            { text: "0", correct: true },
+            { text: "1", correct: false },
+            { text: "-1", correct: false },
+            { text: "不存在", correct: false }
+        ]
+    },
+    {
+        question: "下列哪个是等比数列？",
+        answers: [
+            { text: "1, 3, 5, 7,...", correct: false },
+            { text: "2, 4, 8, 16,...", correct: true },
+            { text: "1, 4, 9, 16,...", correct: false },
+            { text: "1, 1, 2, 3, 5,...", correct: false }
+        ]
+    }
+    // ... 可以继续添加更多题目
+];
